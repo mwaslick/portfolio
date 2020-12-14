71f7b321b2fede7d41b124e9fe9a751e93ea0bf3
Maria Waslick <mariawaslick@gmail.com>
2020-12-14 17:10:09 -0500

    Updates

---
+++ precache-manifest.001ab7faaf2d10898e476fe5eb417584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45b882075137e10cc148b3ceb1b8ffe",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "89916d2d46befe34f66a",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a5b104c73ae42d6cb28d",
+    "url": "/portfolio/static/js/2.29eb80a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.29eb80a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89916d2d46befe34f66a",
+    "url": "/portfolio/static/js/main.f13dbfde.chunk.js"
+  },
+  {
+    "revision": "81a211af5a25ffbee6ce",
+    "url": "/portfolio/static/js/runtime-main.dfb7ec2f.js"
+  }
+]);
